cd22d782811227fe7c11e95711d40751f745d1c2
Nilton Meira <niltonprudys@gmail.com>
2024-11-28 09:53:38 -0300

    feat: images map

--- src/pages/home/components/IconBox.tsx
+++ src/pages/home/components/IconBox.tsx
@@ -1,6 +1,8 @@
-export const IconBox = () => {
-    return(
-        <>
-        </>
-    )
-}
+export const IconBox = ({ image, title }: { image: string; title: string }) => {
+    return (
+        <div className="flex flex-col">
+            <h4>{title}</h4>
+            <img src={image} alt="" />
+        </div>
+    );
+};
--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,6 +1,19 @@
+import { IconBox } from "./components/IconBox"
+
 export const HomePage = () => {
+    let images = ["./CRIN.svg"]
+    let titles = ["Operações"]
+
     return(
         <>
+            <h1>Menu operacional</h1>
+            <div className="grid grid-cols-2 gap-4">
+                {images.map((image, index)=> (
+                    <IconBox key={index} image={image} title={titles[index]}/>
+                ))}
+
+            </div>
+
         </>
     )
 }
